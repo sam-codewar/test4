c484e19389263b5ab5039ea00540202606a52d90
sam-codewar <so872@live.mdx.ac.uk>
2021-11-26 13:11:36 +0400

    more-product

---
+++ products.js
@@ -0,0 +1,46 @@
+let products = [
+            {
+              "id": 1001,
+              "title": "Cat Food, 25lb bag",
+              "description":
+                "A 25 pound bad of <em>irrestitable</em>, " +
+                " organic goodness for your cat.",
+              "price": 20.00,
+              "image": "cat.jpg",
+              "availableInventory": 10,
+              "rating": 3,
+            },
+
+            { "id": 1002,
+              "title": "Yarn",
+              "description": "Yarn, your cart can play with vue",
+              "price": 30.00,
+              "image": "yarn.jpeg",
+              "availableInventory": 5,
+              "rating": 2,
+               },
+
+            { "id": 1003, 
+              "title": "Kitti Litter",
+              "description": "Premium Kitti litter for your cat",
+              "price": 11.00,
+              "image": "cat-litter.jpeg",
+              "availableInventory": 15,
+              "rating": 4 },
+
+            { "id": 1004,
+              "title": "Cat House",
+              "description": "A playing ground for your cat",
+              "price": 12.99,
+              "image": "cat-house.jpeg",
+              "availableInventory": 11,
+              "rating": 4 },
+
+            { "id": 1005,
+              "title": "Laser Pointer",
+              "description": "Drive your cat crazy with this",
+              "price": 49.99,
+              "image": "laser-pointer.jpeg",
+              "availableInventory": 24,
+              "rating": 5},
+          ]
